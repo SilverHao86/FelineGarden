49b5e4780676f7a951647b7dda422fc4b7531cf0
ZaffyPuck <56975883+ZaffyPuck@users.noreply.github.com>
2023-04-17 01:49:27 -0400

    Base Save System
    Data Object - Holds Data
    Data Manager - Uses save manager to move data
    Save Manager - Interface for saving

---
+++ Puzzle RPG/Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+//#if DEBUG
+//Debug.Log("DEBUG");
+//#endif
+//#if DEVELOPMENT_BUILD
+//Debug.Log("DEVELOPMENT_BUILD");
+//#endif
+
+class DataManager
+{
+    private Data currentData;
+    private const int TIME_REQUIRED_TO_SAVE = 10;
+    public DataManager()
+    {
+        SaveManager.Init();
+    }
+    public void NewGame()
+    {
+        currentData = new Data();
+    }
+    public void EndGame(float timer = 0.0f)
+    {
+        currentData.timePlayed = timer;
+        if (currentData.timePlayed > TIME_REQUIRED_TO_SAVE)
+        {
+            Save(currentData, "fin");
+        }
+    }
+    private void Save(Data data, string fileName = "")
+    {
+        SaveManager.NewSave(data, fileName);
+    }
+    private Data Load(string fileName = "")
+    {
+        Data data = SaveManager.Load(fileName);
+        if(!data)
+        {
+            Debug.Log("No data with filename \""+fileName+"\"");
+            return null;
+        }
+        return data;
+    }
+}
---
+++ Puzzle RPG/Assets/Scripts/Data/SaveManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.IO;
+
+public static class SaveManager
+{
+    private static readonly string SAVE_FILEPATH = Application.dataPath + "/Saves/";
+    //private static string SAVE_FILEPATH_FORMATTED; //Directory.GetCurrentDirectory() + "\\Saves\\"; // correct formatting
+    private static readonly string FILE_FORMAT = ".txt";
+    private static DirectoryInfo SAVE_DIRECTORY; // Looking for use, but not much exists rn
+
+    /// <summary>
+    /// Initializes save manager. 
+    /// Finds location of saved files. 
+    /// Creates directory if one does not exist. 
+    /// </summary>
+    public static void Init()
+    {
+        SAVE_DIRECTORY = new DirectoryInfo(SAVE_FILEPATH);
+
+        // Looks for save directory and creates it if one doesn't exist
+       if (!SAVE_DIRECTORY.Exists) //!Directory.Exists(SAVE_FILEPATH)
+        {
+            SAVE_DIRECTORY.Create(); //SAVE_DIRECTORY = Directory.CreateDirectory(SAVE_FILEPATH);
+        }
+    }
+
+    public static void Save(Data data, string fileName)
+    {
+        
+        string filePath = SAVE_FILEPATH + fileName + FILE_FORMAT;
+        File.WriteAllText(filePath, Convert(data));
+    }
+    public static void NewSave(Data data, string fileName = "save")
+    {
+        int saveID = 0;
+        string filePath = SAVE_FILEPATH + fileName + saveID + FILE_FORMAT;
+        // Find a file that doesn't exist
+        while (File.Exists(filePath))
+        {
+            saveID++;
+            filePath = SAVE_FILEPATH + fileName + saveID + FILE_FORMAT;
+        }
+        File.WriteAllText(filePath, Convert(data));
+    }
+    public static Data Load(string fileName)
+    {
+        string filePath = SAVE_FILEPATH + fileName + FILE_FORMAT;
+        if (File.Exists(filePath))
+        {
+            string loadedData = File.ReadAllText(filePath);
+            return Convert(loadedData);
+        }
+        return null;
+    }
+    public static Data LoadNewData(string fileName)
+    {
+        DirectoryInfo dirInfo = new DirectoryInfo(SAVE_FILEPATH);
+        FileInfo[] saveFiles = dirInfo.GetFiles();
+        FileInfo latestSaveFile = null;
+
+        foreach (FileInfo saveFile in saveFiles)
+        {
+            if(latestSaveFile == null)
+            {
+                latestSaveFile = saveFile;
+            }
+
+            if(saveFile.LastWriteTime > latestSaveFile.LastWriteTime)
+            {
+                latestSaveFile = saveFile;
+            }
+        }
+
+        if(latestSaveFile == null) { return null; }
+        
+        string filePath = SAVE_FILEPATH + latestSaveFile.FullName;
+        string loadedData = File.ReadAllText(filePath);
+        return Convert(loadedData);
+    }
+
+    // Helpers - could probably be moved to DataManager
+    private static string Convert(Data dataObject)
+    {
+        return JsonUtility.ToJson(dataObject);
+    }
+    private static Data Convert(string dataObject)
+    {
+        Data data = new Data();
+        JsonUtility.FromJsonOverwrite(dataObject, data);
+        return data;
+    }
+}
